082b0d4f91be29504bae4cf1bcaa8c2e5c87104a
Yogendra Mohan <yogmohan@in.ibm.com>
2019-02-13 07:57:47 +0000

    Updating the code to use the "Hello there" message

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -1,19 +1,18 @@
 var should = require('should');
 var request = require('supertest');
 var server = require('../../../app');
-var util = require('util');
 process.env.A127_ENV = 'test';
 
 describe('controllers', function() {
 
   describe('hello_world', function() {
 
-    describe('GET /hello there', function() {
+    describe('GET /hello', function() {
 
       it('should return a default string', function(done) {
 
         request(server)
-          .get('/hello there')
+          .get('/hello')
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
           .expect(200)
@@ -29,7 +28,7 @@ describe('controllers', function() {
       it('should accept a name parameter', function(done) {
 
         request(server)
-          .get('/hello there')
+          .get('/hello')
           .query({
             name: 'Scott'
           })
